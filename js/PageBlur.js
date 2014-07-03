b988ce0c4d7436d972978b28237c07a50212bbb3
RadiationX <evgenynd@mail.ru>
2014-07-03 20:39:46 +0400

    Increased perfomance and cool position correct

--- js/PageBlur.js
+++ js/PageBlur.js
@@ -1,44 +1,17 @@
-/*function PageBlur(targetScreenId, blurRadius, blurBgImage, positionCorrect, fastBlur) {
-    var targetScreenId = document.getElementById(targetScreenId);
-    html2canvas(targetScreenId, {
-        onrendered: function (canvas) {
-            var start = new Date();
-            var canvasImg = document.body.appendChild(canvas);
-            canvasImg.id = 'canvasImg';
-
-            var height = targetScreenId.offsetHeight;
-            var width = targetScreenId.offsetWidth;
-            if (fastBlur) {
-                integralBlurCanvasRGB(canvasImg.id, 0, 0, width, height, blurRadius, 1);
-            } else {
-                stackBlurCanvasRGB(canvasImg.id, 0, 0, width, height, blurRadius);
-            }
-            $(blurBgImage).css('background-image', 'url(' + document.getElementById(canvasImg.id).toDataURL("image/png") + ')');
-            var end = new Date();
-            //alert('Скорость ' + (end.getTime() - start.getTime()) + ' мс');
-        }
-    });
-    */
 function PageBlur(setup) {
-    var targetScreenId = document.getElementById(setup.targetScreenId);
-    var blurRadius = setup.blurRadius;
-    var blurBgImage = setup.blurBgImage;
-    var positionCorrect = setup.positionCorrect;
-    var fastBlur = setup.fastBlur;
-    var classes = setup.classes;
-    var blurBgImageC = setup.blurBgImageC;
-    //alert(classes);
+    var targetScreenId = document.getElementById(setup.targetScreenId),
+        blurRadius = setup.blurRadius,
+        blurBgWrap = setup.blurBgWrap,
+        blurBgImage = setup.blurBgImage,
+        positionCorrect = setup.positionCorrect,
+        fastBlur = setup.fastBlur;
 
-
-
+    /* Get element by class */
     if (document.getElementsByClassName) {
-
         getElementsByClass = function (classList, node) {
             return (node || document).getElementsByClassName(classList)
         }
-
     } else {
-
         getElementsByClass = function (classList, node) {
             var node = node || document,
                 list = node.getElementsByTagName('*'),
@@ -55,63 +28,85 @@ function PageBlur(setup) {
                     }
                 }
             }
-
             return result
         }
-    }
+    };
 
-
-
-
-    //alert(targetScreenId + blurRadius + blurBgImage + positionCorrect + fastBlur);
+    /* Create blur image */
+    var arrayWrap = getElementsByClass(blurBgWrap);
+    var arrayWrapLenght = arrayWrap.length;
+    var arrayWrapIndex = arrayWrapLenght - 1;
     html2canvas(targetScreenId, {
         onrendered: function (canvas) {
-            var start = new Date();
-            var canvasImg = document.body.appendChild(canvas);
-            canvasImg.id = 'canvasImg';
-
+            var forblur = document.body.appendChild(canvas);
+            forblur.id = 'temporaryCanvas';
+            forblur.style.display = 'none';
             var height = targetScreenId.offsetHeight;
             var width = targetScreenId.offsetWidth;
             if (fastBlur) {
-                integralBlurCanvasRGB(canvasImg.id, 0, 0, width, height, blurRadius, 1);
+                integralBlurCanvasRGB(forblur.id, 0, 0, width, height, blurRadius, 1);
             } else {
-                stackBlurCanvasRGB(canvasImg.id, 0, 0, width, height, blurRadius);
+                stackBlurCanvasRGB(forblur.id, 0, 0, width, height, blurRadius);
             }
-            $(blurBgImage).css('background-image', 'url(' + document.getElementById(canvasImg.id).toDataURL("image/png") + ')');
-            var end = new Date();
-            //alert('Скорость ' + (end.getTime() - start.getTime()) + ' мс');
+            var blured = document.getElementById(forblur.id);
+            var newCanvas;
+            var conte;
+            for (; arrayWrapIndex > -1; arrayWrapIndex--) {
+                newCanvas = document.createElement('canvas');
+                newCanvas.id = blurBgImage + arrayWrapIndex;
+                newCanvas.className = blurBgImage;
+                newCanvas.style.position = 'absolute';
+                newCanvas.style.left = '0px';
+                if (getComputedStyle(arrayWrap[arrayWrapIndex], '').bottom != 'auto') {
+                    newCanvas.style.bottom = '0px';
+                } else {
+                    newCanvas.style.top = '0px';
+                }
+                conte = newCanvas.getContext('2d');
+                newCanvas.width = blured.width;
+                newCanvas.height = blured.height;
+                conte.drawImage(blured, 0, 0);
+                arrayWrap[arrayWrapIndex].id = blurBgWrap + arrayWrapIndex;
+                arrayWrap[arrayWrapIndex].appendChild(newCanvas);
+            }
+            if(arrayWrapIndex == -1){
+                document.getElementById('loading').style.opacity = '0.0';
+            }
         }
     });
 
+    /* Position correct */
     if (positionCorrect) {
-            var arrClass = getElementsByClass(blurBgImageC);
-            var arrClassLenght = arrClass.length;
-        window.onscroll = function () {
-            var arrClassNumber = arrClassLenght - 1;
+        function posCorrect() {
+            var posCorrecElement;
             var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-            var positionTop = -scrolled+targetScreenId.offsetTop;
-            var positionLeft = targetScreenId.offsetLeft;
-            while (arrClassNumber > -1) {
-                var element = document.getElementById(arrClass[arrClassNumber].id = arrClassNumber);
-                element.style.backgroundPosition = ( positionLeft) + 'px ' + (positionTop) + 'px';
-                arrClassNumber = arrClassNumber - 1;
+            var y;
+            var wrapPos;
+            var position;
+            arrayWrapIndex = arrayWrapLenght - 1;
+            for (; arrayWrapIndex > -1; arrayWrapIndex--) {
+                posCorrecElement = document.getElementById(blurBgImage + arrayWrapIndex);
+                wrapPos = arrayWrap[arrayWrapIndex];
+                var kar = getComputedStyle(wrapPos, '').bottom;
+                y = wrapPos.offsetTop;
+                if (kar != 'auto') {
+                    position = posCorrecElement.offsetHeight - document.body.clientHeight - scrolled + parseInt(kar, 10);
+                } else {
+                    position = -y - scrolled;
+                }
+                posCorrecElement.style.webkitTransform = 'translate3d(0,' + (position) + 'px,0)';
+                posCorrecElement.style.mozTransform = 'translate3d(0,' + (position) + 'px,0)';
+                posCorrecElement.style.msTransform = 'translate(0,' + (position) + 'px)';
+                posCorrecElement.style.oTransform = 'translate3d(0,' + (position) + 'px,0)';
+                posCorrecElement.style.transform = 'translate(0,' + (position) + 'px)';
             }
         };
+        setTimeout(function () {
+            posCorrect();
+        }, 100);
     };
-    /*if (positionCorrect) {
-        var attachment = $(blurBgImage).css("background-attachment");
-        var tsPos;
-        var scrolled;
-        window.onscroll = function () {
-            tsPos = $(targetScreenId).offset();
-            scrolled = window.pageYOffset || document.documentElement.scrollTop;
-            if (attachment === 'fixed') {
-                var pos = '' + (tsPos.left) + 'px ' + (tsPos.top - scrolled) + 'px';
-            } else {
-                var pos = '' + (-$(blurBgImage).position().left + tsPos.left) + 'px ' + (-$(blurBgImage).position().top + tsPos.top - scrolled) + 'px';
-            };
-            $(blurBgImage).css('background-position', pos);
-        };
+
+    window.onscroll = function () {
+        posCorrect();
     };
-    */
 };
