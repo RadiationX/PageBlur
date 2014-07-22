c7172e021be43fc78e7c4a1c10d207190847ebe8
RadiationX <evgenynd@mail.ru>
2014-07-22 15:12:45 +0400

    upd

--- js/PageBlur.js
+++ js/PageBlur.js
@@ -3322,16 +3322,8 @@ function BlurStack() {
     };
 })(window, document);
 /*PageBlur*/
-function PageBlur(setup) {
-    var targetScreenId = document.getElementById(setup.targetScreenId),
-        blurRadius = setup.blurRadius,
-        blurBgWrap = setup.blurBgWrap,
-        blurBgImage = setup.blurBgImage,
-        positionCorrect = setup.positionCorrect,
-        fastBlur = setup.fastBlur,
-        clean = setup.clean;
-
-    /* Get element by class */
+function PageBlur(options, custScript) {
+    //Получение всех элементов по заданному классу
     if (document.getElementsByClassName) {
         getElementsByClass = function (classList, node) {
             return (node || document).getElementsByClassName(classList)
@@ -3357,94 +3349,89 @@ function PageBlur(setup) {
         }
     };
 
-    /* Create blur image */
-    var arrayWrap = getElementsByClass(blurBgWrap);
-    var arrayWrapLenght = arrayWrap.length;
-    var arrayWrapIndex = arrayWrapLenght - 1;
-    html2canvas(targetScreenId, {
-        logging: true,
-        onrendered: function (canvas) {
-            var forblur = document.body.appendChild(canvas);
-            forblur.id = 'temporaryCanvas';
-            //forblur.style.display = 'none';
-            var height = targetScreenId.offsetHeight;
-            var width = targetScreenId.offsetWidth;
-            if (fastBlur) {
-                integralBlurCanvasRGB(forblur.id, 0, 0, width, height, blurRadius, 1);
-            } else {
-                stackBlurCanvasRGB(forblur.id, 0, 0, width, height, blurRadius);
-            }
-            var blured = document.getElementById(forblur.id);
-            var newCanvas;
-            var conte;
-            for (; arrayWrapIndex > -1; arrayWrapIndex--) {
-                newCanvas = document.createElement('canvas');
+    //Если при вызове функции не указан ни один параметр, создается объект
+    if (typeof (options) === 'undefined') {
+        var options = {};
+    }
+    var tScreen = document.getElementById(options.tScreen) || document.getElementById('screen'), //id элемента, который нужно размыть
+        wrapBlur = options.wrapBlur || 'wrapBlur', //Класс элементв в котором создается размытый элемент
+        radius = Math.floor(options.radius) || 10, //Радиус размытия
+        opti = options.opti || false, //Оптимизация
+        kOpti = Math.floor(options.kOpti) || 100, //Коэффициент оптимизации
+        algorithm = options.algorithm || 'stack', //Выбор алгоритма размытия stack или integral
+        decrease = 1, //Коэффициент уменьшения изображения по умолчанию
+        classArray = getElementsByClass(wrapBlur), //Получение массива классов
+        classArrayLenght = classArray.length, //Определение длины массива
+        classArrayIndex = classArrayLenght - 1;
 
-                newCanvas.id = blurBgImage + arrayWrapIndex;
-                if (clean) {
-                    document.getElementById(newCanvas.id).parentNode.removeChild(document.getElementById(newCanvas.id));
-                }
-                newCanvas.className = blurBgImage;
-                newCanvas.style.position = 'absolute';
-                newCanvas.style.left = '0px';
-                if (getComputedStyle(arrayWrap[arrayWrapIndex], '').bottom != 'auto') {
-                    newCanvas.style.bottom = '0px';
-                } else {
-                    newCanvas.style.top = '0px';
-                }
-                conte = newCanvas.getContext('2d');
-                newCanvas.width = blured.width;
-                newCanvas.height = blured.height;
-                conte.drawImage(blured, 0, 0);
-                arrayWrap[arrayWrapIndex].id = blurBgWrap + arrayWrapIndex;
-                arrayWrap[arrayWrapIndex].appendChild(newCanvas);
+    //Функция оптимизации
+    function optimize() {
+        if (opti) {
+            //Определение диапазона вводимых чисел
+            if (kOpti > 100) {
+                kOpti = 100;
+            } else if (kOpti < 1) {
+                kOpti = 1;
             }
-            if (clean) {
-                document.body.removeChild(document.getElementById(forblur.id));
-            };
+            //Коэффициент уменьшения изображения (100/50=2, т.е изображение уменьшится в 2 раза)
+            decrease = (radius / kOpti);
+
+            if (decrease < 1) {
+                decrease = 1;
+            }
+            //И радиус тоже уменьшаем
+            radius = radius / decrease;
         }
-    });
+    }
 
-    /* Position correct */
-    if (positionCorrect) {
-        function posCorrect() {
-            var posCorrecElement,
-                scrolled = window.pageYOffset || document.documentElement.scrollTop,
-                y,
-                wrapPos,
-                positionY,
-                positionX;
-            arrayWrapIndex = arrayWrapLenght - 1;
-            for (; arrayWrapIndex > -1; arrayWrapIndex--) {
-                posCorrecElement = document.getElementById(blurBgImage + arrayWrapIndex);
-                wrapPos = arrayWrap[arrayWrapIndex];
-                var kar = getComputedStyle(wrapPos, '').bottom;
-                y = wrapPos.offsetTop;
-                positionX = -wrapPos.offsetLeft - targetScreenId.offsetLeft;
-                if (kar != 'auto') {
-                    positionY = posCorrecElement.offsetHeight - document.body.clientHeight - scrolled + parseInt(kar, 10);
-                } else {
-                    positionY = -y - scrolled;
+    //Функция размытия элемента
+    function blur(c, w, h, r) {
+        if (algorithm === 'integral') {
+            integralBlurCanvasRGB(c, 0, 0, w, h, r);
+        } else if (algorithm === 'stack') {
+            stackBlurCanvasRGB(c, 0, 0, w, h, r);
+        }
+    }
+    html2canvas(tScreen, {
+        onrendered: function (canvas) {
+            optimize();
+            var width = canvas.width, //Настоящая ширина
+                height = canvas.height, //Настоящая высота
+                decWidth = Math.floor(width / decrease), //Уменьшенная ширина
+                decHeight = Math.floor(height / decrease), //Уменьшенная высота
+                tempCanvas = document.body.appendChild(canvas), //Временный canvas, с неразмытым изображением
+                fbCanvas = document.createElement('canvas'); //Создаём новый canvas для размытия, далее fb
+            tempCanvas.style.display = 'none'; //Скрываем временный canvas
+            tempCanvas.id = 'tc';
+            fbCanvas.width = decWidth; //Уменьшаем ширину fb
+            fbCanvas.height = decHeight; //Уменьшаем высоту fb
+            fbCanvas.id = 'fb'; //Добавляем id
+            fbCanvas.style.display = 'none'; //И fb тоже скрываем
+            fbCanvas.getContext('2d').drawImage(tempCanvas, 0, 0, decWidth, decHeight); //В fb помещаем изображение из tempCanvas с уменьшенными размерами 
+            document.body.appendChild(fbCanvas); //Добавляем fb на страницу
+            blur(fbCanvas.id, decWidth, decHeight, radius); //Передаём все параметры для размытия 
+            for (; classArrayIndex > -1; classArrayIndex--) {
+                if (document.body.getAttribute('data-clean') === 'true') {
+                    classArray[classArrayIndex].removeChild(document.getElementById('blured' + classArrayIndex));
                 }
-                posCorrecElement.style.webkitTransform = 'translate(' + positionX + 'px,' + (positionY) + 'px)';
-                posCorrecElement.style.mozTransform = 'translate(' + positionX + 'px,' + (positionY) + 'px)';
-                posCorrecElement.style.msTransform = 'translate(' + positionX + 'px,' + (positionY) + 'px)';
-                posCorrecElement.style.oTransform = 'translate(' + positionX + 'px,' + (positionY) + 'px)';
-                posCorrecElement.style.transform = 'translate(' + positionX + 'px,' + (positionY) + 'px)';
+                var finalCanvas = document.createElement('canvas'); //Создаем новый, финальный canvas каждый, далле final
+                finalCanvas.width = width; //Увеличиваем ширину final
+                finalCanvas.height = height; //Увеличиваем высоту final
+                finalCanvas.id = 'blured' + classArrayIndex; //blured0,blured1 и т.д
+                finalCanvas.getContext('2d').drawImage(fbCanvas, 0, 0, width, height); //В final помещаем изображение из fb с увеличенными размерами
+
+
+                classArray[classArrayIndex].appendChild(finalCanvas); //Добавление final в каждый указанный элемент  
             }
-        };
-        setTimeout(function () {
-            posCorrect();
-        }, 100);
-    };
-
-    window.onscroll = function () {
-        posCorrect();
-    };
+            document.body.setAttribute('data-clean', 'true');
+            if (document.body.getAttribute('data-clean') === 'true') {
+                document.body.removeChild(document.getElementById('fb'));
+                document.body.removeChild(document.getElementById('tc'));
+            }
+        }
+    })
+    //callback
+    if (custScript && typeof (custScript) === 'function') {
+        custScript();
+    }
 };
-
-/*
-Use html2canvas - http://html2canvas.hertzen.com
-    StackBlur - http://www.quasimondo.com/StackBlurForCanvas/StackBlurDemo.html
-    IntegralImageBlur - http://www.quasimondo.com/IntegralImageForCanvas/IntegralImageBlurDemo.html
-*/
